a1bf0133ade4f32a5900d94faf2abec23e6be08c
Rodrigo <rodrigo.teixeira@sou.unifeob.edu.br>
2025-05-06 20:00:00 -0300

    chore: Minor update for testing purposes

---
+++ lib/src/pages/admin_page.dart
@@ -0,0 +1,74 @@
+import 'package:app_fluxolivrep/src/widget/admin_widget.dart';
+import 'package:flutter/material.dart';
+
+class AdminPage extends StatelessWidget {
+  const AdminPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/img_fundologin.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context); 
+                  },
+                  child: const Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.white,
+                    size: 21,
+                  ),
+                ),
+                const SizedBox(width: 50),
+                const Text(
+                  'ADMINISTRADOR',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                children: [
+                  AdminWidget(
+                    title: 'Usuários',
+                    imagePath: 'assets/images/user_admin.png',
+                    rota: '', 
+                  ),
+                  AdminWidget(
+                    title: 'Relatórios',
+                    imagePath: 'assets/images/report_admin.png',
+                    rota: '', 
+                  ),
+                  AdminWidget(
+                    title: 'Configurações',
+                    imagePath: 'assets/images/config_admin.png',
+                    rota: '', 
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
